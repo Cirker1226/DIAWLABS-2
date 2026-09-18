465d5384ace5dbc48e216650edd51779b38c8cd8
Matheus Henrique <matheus.privadotvsmart@gmail.com>
2026-09-18 17:31:02 -0300

    primeiro commit atividade de Spring-Boot

---
+++ src/main/java/ricoy/network/diawlabs/DiawlabsApplication.java
@@ -0,0 +1,13 @@
+package ricoy.network.diawlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiawlabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiawlabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ricoy/network/diawlabs/DiawlabsApplicationTests.java
@@ -0,0 +1,13 @@
+package ricoy.network.diawlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiawlabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
